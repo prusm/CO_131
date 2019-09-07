5443ee8d9715d567ca58e8b3704c97d69afff49e
Malgorzata Prus <malgorzata.prus@bluesoft.com>
2019-09-07 18:27:55 +0200

    Your Message

---
+++ CO_131/gallery.php
@@ -0,0 +1,19 @@
+<?php
+$folder_path = 'common/images/resources/'; //image folder path
+
+$folder = opendir($folder_path);
+
+ while (false !== ($entry = readdir($folder))) {
+	if ($entry != "." && $entry != ".." && $entry != "Thumb.db") {
+
+		$file_path = $folder_path.$entry;
+		$ext = strtolower(pathinfo($file_path, PATHINFO_EXTENSION));
+		if($ext=='jpg' || $ext =='png' || $ext == 'gif')
+		{
+			echo '<img src="'.$file_path.'" />';
+		}
+	}
+}
+
+closedir($folder );
+?>
